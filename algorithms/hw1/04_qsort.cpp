c820629bbe06893a748984e47456c624c5d89fd5
Paul Ivanov <wagenod93@mail.ru>
2022-09-27 17:55:51 +0300

    04 quick sort works too slow

--- algorithms/hw1/04_qsort.cpp
+++ algorithms/hw1/04_qsort.cpp
@@ -2,37 +2,66 @@
 #include <vector>
 #include <string>
 #include <stdlib.h>
+#include <ctime>
+#include <tuple>
 
 using namespace std;
 
 void print_vector(vector<int>& vec, string msg){
-
-    cout << msg << endl;
+    cout << endl << msg << endl;
     for (const auto& item: vec )
         cout << item << " ";
     cout << endl;
 }
 
+void swap_items(vector<int>& v, int i, int j){
+    int buf = v[i];
+    v[i] = v[j];
+    v[j] = buf;
+}
 
-int split(vector<int>& v, int left_idx, int right_idx, int treshold){ // [left_idx, right_idx)
-    int m = left_idx;
+
+tuple<int, int> split(vector<int>& v, int left_idx, int right_idx, int treshold){ // [left_idx, right_idx)
+    int m1 = left_idx, m2 = left_idx;
     for (int i = left_idx; i < right_idx; ++i){
         if (v[i] < treshold){
-            if ( i != m){
-                int buf = v[i];
-                v[i] = v[m];
-                v[m] = buf;
+            if (m2 > m1){
+                for (int d = i -1; d >= m1; --d){
+                    swap_items(v, d, d + 1);
+                }
+            } else if (i != m1){
+                swap_items(v, m1, i);
             }
-            m++;
+            m1++;
+            m2++;
+        } else if (v[i] == treshold) {
+            swap_items(v, m2, i);
+            m2++;
         }
+
+        //print_vector(v, "");
+        
     }
-    return m;
+    return {m1, m2};
 }
 
 
 void quicksort(vector<int>& v, int left_idx, int right_idx){
     if (right_idx - left_idx <= 1)
-        return 
+        return;
+
+    srand(time(NULL));
+    //cout << "depth = " << depth << endl;
+    //print_vector(v, "v=");
+    int rand_idx = left_idx + rand() % (right_idx - left_idx);
+    //cout << "rand_idx " << rand_idx << endl;
+    int treshold = v[rand_idx];
+    //cout << "treshold " << treshold << endl;
+    int m1, m2;
+    tie(m1, m2) = split(v, left_idx, right_idx, treshold);
+    //cout << "m1=" << m1 << " m2=" << m2 << endl;
+    quicksort(v, left_idx, m1);  // [left_idx; m1)
+    quicksort(v, m2, right_idx); // [m2; right_idx)
 }
 
 
@@ -45,8 +74,9 @@ int main(){
     for(auto& item: v){
         cin >> item;
     }
-
-    split(v, 0, n, 3);
+    //vector<int> t = {2, 8, 4, 7, 3, 2, 3, 6};
+    //split(t, 0, 8, 3);
+    quicksort(v, 0, n);
     print_vector(v, "");
     system("pause");
     return 0; 
