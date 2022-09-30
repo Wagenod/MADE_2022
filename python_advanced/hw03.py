1516980b16dbee75f3881c91a2f9d6eadb04b2d2
Paul Ivanov <wagenod93@mail.ru>
2022-09-30 15:56:48 +0300

    dolved 4 hm

--- python_advanced/hw03.py
+++ python_advanced/hw03.py
@@ -1,33 +1,46 @@
-# 1. Написать функцию, которая в качестве аргументов принимает строку json, список полей, которые необходимо 
-# обработать, список имён, которые нужно найти и функцию-обработчика имени, который срабатывает, 
-# когда в каком-либо поле было найдено ключевое имя. 
-
-#Функция, должна принимать строку, в которой содержится json, и произвести парсинг этого json. 
-#Упростим немного и представим, что json представляет из себя только коллекцию ключей-значений. 
-#Причём ключами и значениями являются только строки.
-
-''' def parse_json(json_str: str, required_fields=None, keywords=None, keyword_callback)
-    Например, представим, что 
-            json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
-            required_fields = ["key1"] 
-            keywords = ["word2"]
-    Тогда keyword_callback будет вызвана только для слова 'word2' для ключа 'key1'.
-'''
 import json
+import time
+import random
+
+# 1. Написать функцию, которая в качестве аргументов принимает строку json,
+# список полей, которые необходимо обработать, список имён, которые нужно
+# найти и функцию-обработчика имени, который срабатывает, когда в каком-либо
+# поле было найдено ключевое имя.
+
+# Функция, должна принимать строку, в которой содержится json, и произвести
+# парсинг этого json. Упростим немного и представим, что json представляет
+# из себя только коллекцию ключей-значений. Причём ключами и значениями
+# являются только строки.
+
+''' def parse_json(json_str: str,
+                    required_fields=None,
+                    keywords=None,
+                    keyword_callback)
+
+    Например, представим, что
+            json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
+            required_fields = ["key1"]
+            keywords = ["word2"]
+    Тогда keyword_callback будет вызвана только для слова word2 для ключа key1.
+'''
+
 
 stats = {}
 
+
 def stats_counter(word):
     if word not in stats:
-        stats[word]=1
+        stats[word] = 1
     else:
         stats[word] += 1
 
 
-def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=None):
+def parse_json(json_str: str, keyword_callback,
+               required_fields=None, keywords=None):
     if not json_str or required_fields is None or keywords is None:
-        raise ValueError("Arguments: json_str, required_fields, keywords must take some value, not None or empty value")
-    
+        raise ValueError("Arguments json_str, required_fields, keywords \
+                        must take some value, not None or empty value")
+
     if not callable(keyword_callback):
         raise TypeError("Argument keyword_callback must be callable")
 
@@ -38,23 +51,21 @@ def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=N
             if kw in json_dict[field].split(" "):
                 keyword_callback(kw)
 
+
 # Example
 json_str = '{"key1": "Word1 word2", "key2": "word2 word3"}'
-#parse_json(json_str, stats_counter, required_fields=['key1'])
+# parse_json(json_str, stats_counter, required_fields=['key1'])
 parse_json(json_str, stats_counter, required_fields=['key1', 'key2'], keywords=[])
 
 
-
-
-#2. Написать декоратор, который считает среднее время выполнения последних k вызовов.
-import time
-import random
+# 2.Написать декоратор, который считает среднее время выполнения последних k вызовов.
 
 def mean(k):
     func_duration = []
+
     def mean_decorator(func):
         def wrapper(*args, **kwargs):
-            
+
             time_start = time.time()
             res = func(*args, **kwargs)
             time_end = time.time()
@@ -62,25 +73,25 @@ def mean(k):
             nonlocal func_duration
             func_duration.append(time_end - time_start)
             if len(func_duration) > k:
-                func_duration = func_duration[1:] 
-            print("mean duration= {:.2f}\n".format(sum(func_duration)/len(func_duration)))
+                func_duration = func_duration[1:]
+            print("mean duration= {:.2f}\n".format(
+                  sum(func_duration)/len(func_duration)))
+
             return res
         return wrapper
     return mean_decorator
-
-
 
 
 @mean(10)
 def foo(arg1):
     time.sleep(random.random())
 
+
 @mean(2)
 def boo(arg1):
     pass
 
+
 for _ in range(100):
     foo("Walter")
     boo("lop")
-
-
---
+++ python_advanced/hw04.py
@@ -0,0 +1,97 @@
+from ast import Num
+from typing import Iterable, List, Set, Tuple
+from itertools import zip_longest
+from unittest import result
+from typing import Union
+from collections.abc import Sequence
+from functools import total_ordering
+
+
+class CustomList(list):
+    def __init__(self, items):
+        if not (isinstance(items, list) or isinstance(items, tuple)):
+            raise TypeError("Argument must be list or tuple")
+
+        self.items = list(items)
+
+    def __sub__(self, another):
+        return CustomList([x - y for x,y in zip_longest(self, another, fillvalue=0)])
+
+    def __rsub__(self, another):
+        return CustomList([y - x for x,y in zip_longest(self, another, fillvalue=0)])
+
+    def __add__(self, another):
+        return CustomList([x + y for x,y in zip_longest(self, another, fillvalue=0)])
+
+    def __radd__(self, another):
+        return self.__add__(another)
+        
+    def __iter__(self):
+        self.__idx = -1 
+        return self
+
+    def __next__(self):
+        self.__idx += 1
+        if self.__idx >= len(self.items):
+            self.__idx = -1
+            raise StopIteration
+        else:
+            return self.items[self.__idx]
+
+    def __repr__(self):
+        return "CustomList({})".format(self.items)
+
+    def __str__(self):
+        return "CustomList items: {} sum= {}".format(self.items, sum(self.items))
+
+    def __eq__(self, another) -> bool:
+        return sum(self.items) == sum(another.items)
+
+    def __ne__(self, another) -> bool:
+        return sum(self.items) != sum(another.items)
+
+    def __le__(self, another) -> bool:
+        return sum(self.items) <= sum(another.items)
+    
+    def __ge__(self, another) -> bool:
+        return sum(self.items) >= sum(another.items)
+
+    def __lt__(self, another) -> bool:
+        return sum(self.items) < sum(another.items)
+    
+    def __gt__(self, another) -> bool:
+        return sum(self.items) > sum(another.items)
+
+
+# -
+assert repr((CustomList([1, 3, 6]) - CustomList([0, 4]))) == repr(CustomList([1, -1, 6]))
+assert repr(([5, 0, 6] - CustomList([2, 1]))) == repr(CustomList([3, -1, 6]))
+assert repr((CustomList([2, 1]) - [5, 0, 6])) == repr(CustomList([-3, 1, -6]))
+assert repr((CustomList([2, 1]) - [])) == repr(CustomList([2, 1]))
+assert repr((CustomList([]) - [])) == repr(CustomList([]))
+assert repr((CustomList([]) - [2, 1])) == repr(CustomList([-2, -1]))
+
+# +
+assert repr((CustomList([1, 3, 6]) + CustomList([0, 4]))) == repr(CustomList([1, 7, 6]))
+assert repr([5, 0, 6] + CustomList([2, 1])) == repr(CustomList([7, 1, 6]))
+assert repr((CustomList([2, 1]) + [5, 0, 6])) == repr(CustomList([7, 1, 6]))
+assert repr((CustomList([]) + [])) == repr(CustomList([]))
+assert repr((CustomList([]) + [2, 1])) == repr(CustomList([2, 1]))
+
+# ==
+
+assert CustomList([2, 1, 4]) == CustomList([7])
+assert CustomList([2, 1, 4, 5]) != CustomList([7])
+assert CustomList([2, 1, 4, 5]) > CustomList([7])
+assert CustomList([2, 1, 4, 5]) < CustomList([7, 10])
+assert CustomList([2, 1]) <= CustomList([3])
+assert CustomList([2, 1]) >= CustomList([3])
+assert CustomList([]) == CustomList([])
+assert CustomList((2,)) == CustomList([1, 1])
+
+# __str__
+assert str(CustomList([12, 34, 5.2])) == "CustomList items: [12, 34, 5.2] sum= 51.2"
+print(CustomList({"123", 34, 5.2}))
+
+
+
