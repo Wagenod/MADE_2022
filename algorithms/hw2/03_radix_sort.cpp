2afbe05057722a21dba7c7f6dd68391919b9af93
Paul Ivanov <wagenod93@mail.ru>
2022-10-12 17:13:11 +0300

    commit

--- algorithms/hw2/03_radix_sort.cpp
+++ algorithms/hw2/03_radix_sort.cpp
@@ -4,9 +4,7 @@
 
 using namespace std;
 
-const int ALPHABET_CARD = 26;
-const int ASCII_BEGIN_CODE = 97;
-const int ASCII_END_CODE = 122;
+const int ALPHABET_CAPACITY = 26;
 
 template<typename T>
 void print_vector(const vector<T>& vec){
@@ -16,15 +14,31 @@ void print_vector(const vector<T>& vec){
     cout << endl;
 }
 
-void radix_sort(const vector<string>& vstr, int k){
-    for (int i = 1; i <= k; i++){
-        vector<int> cnt(ALPHABET_CARD);
-        for (const string& str: vstr){
-            cnt[int(str[str.size() - i]) - ASCII_BEGIN_CODE]++;
-        }
-        print_vector(cnt);
+vector<int> radix_sort(const vector<string>& vstr, int k, int m, int n){
+    vector<int> P(n); // вектор перестановок для исходных строк
+    for (size_t j = 0; j < n; j++){
+        P[j] = j;
     }
-    
+
+    for (int i = 1; i <= k; i++){
+        vector<int> cnt(ALPHABET_CAPACITY);
+        for (size_t l = 0; l < n; l++){
+            
+            cnt[int(vstr[P[l]][m - i] - 'a')]++;
+        }
+
+        vector<int> shifts(ALPHABET_CAPACITY);
+        for (size_t j = 1; j < shifts.size(); j ++){
+            shifts[j] = shifts[j - 1] + cnt[j - 1];
+        }
+
+        vector<int> P_old(P.begin(), P.end());
+        for (size_t l = 0; l < n; l++){
+            int cnt_idx = int(vstr[P_old[l]][m - i]) - 'a';
+            P[shifts[cnt_idx]++] = P_old[l];
+        }
+    }
+    return P;
 }
 
 
@@ -39,11 +53,12 @@ int main(){
         getline(cin, item);
     }
 
-    radix_sort(strs, k);
-    // for (int l = 0; l < m; l++){
+    vector<int> P = radix_sort(strs, k, m, n);
 
-    // }
-
-
+    for(const int& p: P){
+        cout << strs[p] << endl;
+    }
+    
+    system("pause");
     return 0;
 }
