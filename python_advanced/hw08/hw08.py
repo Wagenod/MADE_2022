81a41e2a9f550c64fc228ed0d2f221c5a0199da4
Paul Ivanov <wagenod93@mail.ru>
2022-10-21 11:33:43 +0300

    better code style in hw08

--- python_advanced/hw08/hw08.py
+++ python_advanced/hw08/hw08.py
@@ -15,25 +15,16 @@ def find_anagrams(text: str, pattern: str) -> List[int]:
             if letter not in pattern_cnt:
                 shift = k
                 break
-            else:
-                if letter not in tmp_cnt:
-                    tmp_cnt[letter] = 1
-                else:
-                    tmp_cnt[letter] += 1
-
-                if tmp_cnt[letter] > pattern_cnt[letter]:
-                    break
+            tmp_cnt[letter] = tmp_cnt[letter] + 1 if letter in tmp_cnt else 1
+            if tmp_cnt[letter] > pattern_cnt[letter]:
+                shift = 1
+                break
         else:
             anagrams.append(i)
     return anagrams
 
 
-# def find_anagrams(text: str, pattern: str) -> List[int]:
-#     anagrams = []
-#     pattern_cnt = Counter(pattern)
-
-#     for i in range(0, len(text) - len(pattern) + 1): # O(len(text))
-#         if Counter(text[i: i + len(pattern)]) == pattern_cnt: # O(len(pattern))
-#             anagrams.append(i)
-#     return anagrams
-    
+if __name__ == "__main__":
+    txt = "abracadabra"
+    search_str = "abra"
+    print("{} indexes in {}: {}".format(search_str, txt, find_anagrams(txt, search_str)))
