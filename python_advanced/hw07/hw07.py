fa47a609921cd6ed02f2aa27dcd4c762f1f7fb8e
Paul Ivanov <wagenod93@mail.ru>
2022-11-21 17:53:08 +0300

    21.11

--- python_advanced/hw07/hw07.py
+++ python_advanced/hw07/hw07.py
@@ -1,4 +1,4 @@
-import json, csv
+import json, csv, os
 
 
 class BaseReader:
@@ -10,17 +10,21 @@ class BaseWriter:
     def dump(data, fileobj):
         pass
 
+# txt
 
 class TxtReader(BaseReader):
-    def read(self, fileobj, *args, **kwargs):
+    def read(self, fileobj):
         return fileobj.read()
 
 
 class TxtWriter(BaseWriter):
-    def write(self, data, fileobj, *args, **kwargs):
+    def dump(self, data, fileobj):
         fileobj.write(data)
 
 
+# json
+
+# csv
 
 # использование
 def read_data(fileobj, reader: BaseReader):
@@ -29,9 +33,13 @@ def read_data(fileobj, reader: BaseReader):
 def dump_data(data, fileobj, writer: BaseWriter):
     writer.dump(data, fileobj)
 
+ROOT_DIR = r"python_advanced\\hw07"
 
-with open(r"hw07//1.txt") as f:
+with open(os.path.join(ROOT_DIR, "1.txt")) as f:
     print(read_data(f, TxtReader()))
+
+with open(os.path.join(ROOT_DIR, "2.txt"), 'w') as f:
+    dump_data("dump", f, TxtWriter())
 # dump_data({"x": "1"}, fileobj, writer=JsonWriter())  # в fileobj записывается json {"x": "1"}
 
 # data = read_data(fileobj, reader=JsonReader())
